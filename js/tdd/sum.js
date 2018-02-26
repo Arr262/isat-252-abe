759b8e85460d7be9da7ff424a370ffbd98960e08
unknown <Rahimar@dukes.jmu.edu>
2018-02-25 19:40:42 -0800

    1st attempt

---
+++ js/tdd/sum.js
@@ -0,0 +1,9 @@
+/**
+* a function to add two numbers 
+* @param [number] a the first number to be added
+* @param [number] b the second number to be added
+*/
+function sum(a,b) {
+  return a + b
+}
+module.exports = sum;
