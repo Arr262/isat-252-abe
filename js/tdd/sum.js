b2252a009a1ee10220ad89f475db1e539ad346ee
unknown <Rahimar@dukes.jmu.edu>
2018-02-26 10:53:43 -0800

    sum refactor complete

--- js/tdd/sum.js
+++ js/tdd/sum.js
@@ -1,9 +1,10 @@
 /**
-* a function to add two numbers 
+* a function to add two numbers
 * @param [number] a the first number to be added
-* @param [number] b the second number to be added
+* @param [number] b the second number to be added	
+* return [number]   the sum of a and b	
 */
 function sum(a,b) {
   return a + b
 }
-module.exports = sum;
+module.exports = sum;		
